b248f988ed3a586cd670852d0704c200b6367e23
INSECT5386 <shinnara.yuchan@gmail.com>
2025-02-06 20:05:03 +0700

    huggingface.js 업데이트

--- netlify/functions/huggingface.js
+++ netlify/functions/huggingface.js
@@ -1,7 +1,7 @@
 export async function handler(event, context) {
     const API_KEY = process.env.HUGGINGFACE_API_KEY;
 
-    const response = await fetch("https://api-inference.huggingface.co/models/skt/kogpt2-base-v2", {
+    const response = await fetch("https://api-inference.huggingface.co/models/EleutherAI/gpt-neo-2.7B", {
         method: "POST",
         headers: {
             "Authorization": `Bearer ${API_KEY}`,
