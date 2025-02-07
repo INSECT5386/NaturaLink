cba44077d9bb6c1095606fe886656cc98db34aec
INSECT5386 <shinnara.yuchan@gmail.com>
2025-02-07 16:13:20 +0700

    huggingface.js 업데이트

--- netlify/functions/huggingface.js
+++ netlify/functions/huggingface.js
@@ -4,7 +4,7 @@ export async function handler(event, context) {
     const user_input = JSON.parse(event.body).text;
     const prompt = `User: ${user_input}\nAI:`;
 
-    const response = await fetch("https://api-inference.huggingface.co/models/facebook/blenderbot-3B", {
+    const response = await fetch("https://api-inference.huggingface.co/models/facebook/blenderbot-1B-distill", {
         method: "POST",
         headers: {
             "Authorization": `Bearer ${API_KEY}`,
