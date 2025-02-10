d687f9008450f36ce0f2d3e399b483748f48e4bd
INSECT5386 <shinnara.yuchan@gmail.com>
2025-02-10 22:56:26 +0900

    Update AI3.js

--- netlify/functions/AI3.js
+++ netlify/functions/AI3.js
@@ -14,9 +14,9 @@ export async function handler(event, context) {
                 inputs: user_input,
                 parameters: {
                     max_new_tokens: 50,
-                    temperature: 0.2,
+                    temperature: 0.4,
                     top_p: 0.9,
-                    repetition_penalty: 1.0
+                    repetition_penalty: 1.5
                 }
             })
         });
