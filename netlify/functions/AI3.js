458e418c0e77b5aeeb267c60f95aa4c17a754af0
INSECT5386 <shinnara.yuchan@gmail.com>
2025-02-10 22:19:50 +0900

    Update AI3.js

--- netlify/functions/AI3.js
+++ netlify/functions/AI3.js
@@ -4,7 +4,7 @@ export async function handler(event, context) {
     try {
         const user_input = JSON.parse(event.body).text;
 
-        const response = await fetch("https://api-inference.huggingface.co/models/deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B", {
+        const response = await fetch("https://api-inference.huggingface.co/models/Qwen/Qwen2.5-1.5B-Instruct", { // URL 변경
             method: "POST",
             headers: {
                 "Authorization": `Bearer ${API_KEY}`,
